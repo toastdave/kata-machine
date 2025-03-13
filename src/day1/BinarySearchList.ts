d9497db58ee005d8856b79371640bc863dd77441
DavidPierce24 <pie2ce@gmail.com>
2025-03-12 23:38:53 -0400

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,12 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length;
+    while (lo < hi){
+        const m = Math.floor(lo + (hi-lo)/2);
+        const v = haystack[m];
+        if (v === needle) return true;
+        if (v < needle) lo = m + 1;
+        if (v > needle) hi = m
+    }
+    return false;
 }
