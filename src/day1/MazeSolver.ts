4debb8364b28cc167062b901d91f015f1fba8de8
David Pierce <pie2ce@gmail.com>
2025-11-01 14:17:17 -0400

    maze_solver: base case

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,28 @@
+
+function walk(maze: string[], wall: string, current: Point, end: Point, seen: boolean[][]): boolean {
+    // Base Case
+    // 1. Off The Map
+    if (current.x < 0 || current.x >= maze[0].length || current.y < 0 || current.y >= maze.length){
+        return false
+    }
+    
+    // 2. On A Wall
+    if (maze[current.y][current.x] === wall){
+        return false
+    }
+
+    // 3. At End
+    if (current.y === end.y && current.x === end.x){
+        return true
+    }
+
+    // 4. Seen Before
+    if (seen[current.y][current.x]){
+        return false
+    }
+
+}
+
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
 }
